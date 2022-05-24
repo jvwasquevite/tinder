ccd7f47a78268e021ebfcc39049c13ad79b41afb
João Wasquevite <jvwasquevite@gmail.com>
2022-05-24 16:05:37 -0300

    finished api

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,4 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-export async function handler() {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ msg: 'Hello world' }),
-  }
-}
-
 app.listen(8000, () => console.log('Server is running'))
